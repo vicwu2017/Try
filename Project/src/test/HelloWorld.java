65d77b573362f5cd330ec57c0e461a9139f4a534
vicwu2017 <max9411251@gmail.com>
2018-01-21 18:22:35 +0800

    Try using GitHub to upload the project.

---
+++ Project/src/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+/**
+ * HelloWord
+ * @author Vic
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		String str = "HelloWord!";
+		System.out.println(str);
+	}
+}
